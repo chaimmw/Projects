f3b2ce5c6fb3c43bd65a88aec9bc735c7dec373e
chaimmw <chaimmw86@gmail.com>
2017-07-27 21:24:23 -0400

    C# Project

---
+++ evenFibonaciNumbers/evenFibonaciNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenFibonaciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonaci Fibber = new Fibonaci();
+            List<int> numbers = Fibber.NumbersInSequenc;
+            Fibber.CreateSequence(1, 2);
+            List<int> evenNumbers = Fibber.EvenNumbersInSequence;
+            foreach(int fib in numbers)
+            {
+                Fibber.AddEvenFibonaci(fib);
+            }
+            int totalOfFibs = evenNumbers.Sum();
+            Console.WriteLine(totalOfFibs);
+            Console.ReadLine();
+        }
+        public class Fibonaci
+        {
+            public List<int> NumbersInSequenc = new List<int>();
+            public List<int> EvenNumbersInSequence = new List<int>();
+            public void CreateSequence(int num1, int num2)
+            {
+                int num3 = 0;
+                while(num1 <= 4000000)
+                {
+                    num3 = num1 + num2;
+                    NumbersInSequenc.Add(num1);
+                    num1 = num2;
+                    num2 = num3;
+                    
+                }
+                
+            }
+            public void AddEvenFibonaci(int fib)
+            {
+                if(fib % 2 == 0)
+                {
+                    EvenNumbersInSequence.Add(fib);
+                }
+            }
+        }
+    }
+}
